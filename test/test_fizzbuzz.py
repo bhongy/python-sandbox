321e8f8a9097d58017892288d9419a811973e5d8
Thanik Bhongbhibhat <thanikb@pinterest.com>
2018-09-02 12:18:48 -0700

    Refactor FizzBuzz implementation

--- test/test_fizzbuzz.py
+++ test/test_fizzbuzz.py
@@ -1,36 +1,35 @@
-def fizzbuzz(value):
-  if is_multiple(value, 3) and is_multiple(value, 5):
-    return "FizzBuzz"
-  if is_multiple(value, 3):
-    return "Fizz"
-  if is_multiple(value, 5):
-    return "Buzz"
-  return str(value)
+def fizzbuzz(n):
+    return "Fizz" * (n % 3 == 0) + "Buzz" * (n % 5 == 0) or str(n)
 
-def is_multiple(value, mod):
-  return value % mod == 0
 
 def check_fizzbuzz(value, expected_result):
-  actual = fizzbuzz(value)
-  assert actual == expected_result
+    actual = fizzbuzz(value)
+    assert actual == expected_result
+
 
 def test_returns_1_with_1_passed_in():
-  check_fizzbuzz(1, "1")
+    check_fizzbuzz(1, "1")
+
 
 def test_returns_2_with_2_passed_in():
-  check_fizzbuzz(2, "2")
+    check_fizzbuzz(2, "2")
+
 
 def test_returns_fizz_with_3_passed_in():
-  check_fizzbuzz(3, "Fizz")
+    check_fizzbuzz(3, "Fizz")
+
 
 def test_returns_buzz_with_5_passed_in():
-  check_fizzbuzz(5, "Buzz")
+    check_fizzbuzz(5, "Buzz")
+
 
 def test_returns_fizz_with_6_passed_in():
-  check_fizzbuzz(6, "Fizz")
+    check_fizzbuzz(6, "Fizz")
+
 
 def test_returns_buzz_with_10_passed_in():
-  check_fizzbuzz(10, "Buzz")
+    check_fizzbuzz(10, "Buzz")
+
 
 def test_returns_fizzbuzz_with_15_passed_in():
-  check_fizzbuzz(15, "FizzBuzz")
+    check_fizzbuzz(15, "FizzBuzz")
